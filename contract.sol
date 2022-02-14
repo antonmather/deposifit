f864da0c146ddbd4c5e204dc0a8cdd6a415b7aae
antonmather <14355622+antonmather@users.noreply.github.com>
2022-02-15 09:49:22 +1300

    some implementation changes

--- contract.sol
+++ contract.sol
@@ -2,32 +2,38 @@ pragma solidity ^0.7.0;
 // SPDX-License-Identifier: MIT
 
 import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.7/vendor/SafeMathChainlink.sol";
+//import "@chainlink/contracts/src/v0.7/vendor/SafeMathChainlink.sol";
 // what is SafeMathChainklink.sol?
 import "@chainlink/contracts/src/v0.7/KeeperCompatible.sol";
 
 contract pledge is KeeperCompatibleInterface {
-    using SafeMathChainlink for uint256; // I don't know why but it sounds nice 
+    //using SafeMathChainlink for uint256; // I don't know why but it sounds nice 
 
-    uint256 public immutable steps_target=100000;
-    uint256 public immutable date_target = 1669399641;  //this date is one year from now;
-    uint256 public immutable minimumUSD = 50 * 10 ** 18; //require at least 50 usd in stupid units
+    uint256 private immutable steps_target;
+    uint256 private immutable date_target;
+    address payable immutable private success_destination;
+    
+    // Constants
+    uint256 public constant MINIMUM_PLEDGE = 50 * 10 ** 18; //require at least 50 usd in stupid units
+    uint8 public constant MAX_DONORS = 20; // max number of donors so dont risk gas getting too expensive
+    uint256 public constant SECONDS_IN_DAY = 86400;
+
     //address rinkby_eth_usd = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
-    address immutable kovan_eth_usd = 0x9326BFA02ADD2366b30bacB125260Af641031331;
-    uint8 public immutable max_donors = 20; // max number of donors so dont risk gas getting too expensive
+    address constant kovan_eth_usd = 0x9326BFA02ADD2366b30bacB125260Af641031331;
     
     uint8 public donors_so_far = 0;
     bool public date_reached=false;
     bool public steps_reached=false;
+
+    //Deposit[] deposits;
     
     // a new type to contain all relevant info about a depoit to the contract
-    struct deposit{
+    struct Deposit {
         uint256 amount;
-        address payable success_destination;
         address payable fail_destination;
     }
     
-    mapping (address=>deposit) public address_to_deposit;
+    mapping (address=>Deposit) public address_to_deposit;
     address[] public userAddresses; // this is needed to iterate the mapping. mappings alone cannot be iterated
     
 
@@ -44,7 +50,7 @@ contract pledge is KeeperCompatibleInterface {
     }
     
     // this converts however much eth they spend to usd
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+    function convertToUSD(uint256 ethAmount) public view returns (uint256){
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
@@ -62,33 +68,53 @@ contract pledge is KeeperCompatibleInterface {
     
     // error due to including "public" in constructor: https://ethereum.stackexchange.com/questions/98453/visibility-for-constructor-is-ignored-if-you-want-the-contract-to-be-non-deploy/101614
     
-    constructor(uint256 _steps_target, uint256 _date_target, address payable _success_destination, address payable _fail_destination) payable{
+    constructor(uint256 _steps_target, uint256 _num_days, address payable _success_destination, address payable _fail_destination) public payable {
         //owner = msg.sender; // What does this do?
         
+        // Validate data
         require(_steps_target > 100,"have some ambition");
-        require(_date_target > block.timestamp, "that's in the past" ); // Could query todays date here if it were worth the money to bother
-        
+        require(_num_days >= 30, "goal must be at least 30 days away" ); // Could query todays date here if it were worth the money to bother
+        uint256 valueInUSD = convertToUSD(msg.value);
+        require(valueInUSD >= MINIMUM_PLEDGE, "You need to spend more ETH!");
+
+        // Set success data
         steps_target = _steps_target;
-        date_target = _date_target;
+        date_target = block.timestamp + (_num_days * SECONDS_IN_DAY);
+        success_destination = _success_destination;
         
         // code below same as fund() - see there for comments
-        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
-        address_to_deposit[msg.sender].amount += msg.value; 
-        address_to_deposit[msg.sender].success_destination = _success_destination;
-        address_to_deposit[msg.sender].fail_destination = _fail_destination;
+        
+        Deposit memory initial_deposit = Deposit({amount:msg.value, fail_destination:_fail_destination});
+        address_to_deposit[msg.sender] = initial_deposit;
         userAddresses.push(msg.sender); 
+        //deposits.push(initial_deposit);
+
+        //address_to_deposit[msg.sender].amount += msg.value; 
+        //address_to_deposit[msg.sender].success_destination = _success_destination;
+        //address_to_deposit[msg.sender].fail_destination = _fail_destination;
+        
     }
-    function fund(address payable _success_destination, address payable _fail_destination) external payable{
-        require(donors_so_far<max_donors, "sorry this contract is full");
-        require(getConversionRate(msg.value) >= minimumUSD, "comeon, be generous");
+    
+    
+    function fund(address payable _fail_destination) external payable{
+        require(donors_so_far < MAX_DONORS, "sorry this contract is full");
+        uint256 valueInUSD = convertToUSD(msg.value);
+        require(valueInUSD >= MINIMUM_PLEDGE, "comeon, be generous");
+
+        
         
         if (address_to_deposit[msg.sender].amount==0){ // this person hasn't previously submitted -- min deposit ensures this
-            address_to_deposit[msg.sender].amount += msg.value; /// Will the msg indicate what token too, do we need a dif function for every ERC-20
-            address_to_deposit[msg.sender].success_destination = _success_destination;
-            address_to_deposit[msg.sender].fail_destination = _fail_destination;
-            userAddresses.push(msg.sender); //need an array to record these to iterate the mapping
+            Deposit memory new_deposit = Deposit({amount:msg.value, fail_destination:_fail_destination});
+            address_to_deposit[msg.sender] = new_deposit;
+            userAddresses.push(msg.sender);
+            
+            //address_to_deposit[msg.sender].amount += msg.value; /// Will the msg indicate what token too, do we need a dif function for every ERC-20
+            //address_to_deposit[msg.sender].success_destination = _success_destination;
+            //address_to_deposit[msg.sender].fail_destination = _fail_destination;
+            //userAddresses.push(msg.sender); //need an array to record these to iterate the mapping
         }
         else{
+            // Increase the amount of the existing deposit by value of message
             address_to_deposit[msg.sender].amount += msg.value; /// Will the msg indicate what token too, do we need a dif function for every ERC-20
         }
     }
@@ -100,7 +126,7 @@ contract pledge is KeeperCompatibleInterface {
                 //for every deposit, transfer to success_destination
                 for (uint i=0; i<userAddresses.length; i++) {
                     address add=userAddresses[i];
-                    transfer( address_to_deposit[add].success_destination, address_to_deposit[add].amount); 
+                    transfer( success_destination, address_to_deposit[add].amount); 
             }
         }
         else{
